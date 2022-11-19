036a889d9b86db56dd138e3470bc01a0d1bec145
VicenteJankowski <jxstudiopl@gmail.com>
2022-11-19 16:52:40 +0100

    Initialization of a new app folder structure with Maven

---
+++ src/main/java/pl/admonster/uniqueStringGenerator/UniqueStringGeneratorApplication.java
@@ -0,0 +1,13 @@
+package pl.admonster.uniqueStringGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqueStringGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqueStringGeneratorApplication.class, args);
+	}
+
+}
