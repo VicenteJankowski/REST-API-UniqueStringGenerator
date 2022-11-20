2c1b1406d827e048f64f8925c2c09ab3df7cb340
VicenteJankowski <jxstudiopl@gmail.com>
2022-11-20 12:55:41 +0100

    Added support for UserRequestID

--- src/main/java/pl/admonster/uniqueStringGenerator/UniqueStringGeneratorModel.java
+++ src/main/java/pl/admonster/uniqueStringGenerator/UniqueStringGeneratorModel.java
@@ -2,8 +2,11 @@ package pl.admonster.uniqueStringGenerator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 @Repository
@@ -20,17 +23,31 @@ public class UniqueStringGeneratorModel{
         return str.substring(0, str.length() - 1);
     }
     public int addUserRequestToDB(UserRequest userRequest){
-        return jdbcTemplate.update("INSERT INTO userRequests(userChars, maxLength, minLength, howManyWanted, jobFinished) " +
-                        "VALUES (?, ?, ?, ?, ?)",
-                        parseUserCharsToDB(userRequest.getUserChars()),
-                        userRequest.getMaxLength(),
-                        userRequest.getMinLength(),
-                        userRequest.getHowManyWanted(),
-                        UserRequest.Status.NOT_STARTED
-                );
+
+        GeneratedKeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+        String sql = "INSERT INTO userRequests(userChars, maxLength, minLength, howManyWanted, jobFinished) VALUES (?, ?, ?, ?, ?)";
+
+        jdbcTemplate.update(conn -> {
+
+            PreparedStatement preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setString(1, parseUserCharsToDB(userRequest.getUserChars()));
+            preparedStatement.setInt(2, userRequest.getMaxLength());
+            preparedStatement.setInt(3, userRequest.getMinLength());
+            preparedStatement.setInt(4, userRequest.getHowManyWanted());
+            preparedStatement.setObject(5, UserRequest.Status.NOT_STARTED);
+
+            return preparedStatement;
+
+        }, generatedKeyHolder);
+
+        userRequest.setId(generatedKeyHolder.getKey().intValue());
+        System.out.println("USerRequestID=" + userRequest.getId());
+
+        return 1;
     }
 
-    public int changeUserRequestStatusInDB(boolean status, int requestId){
+    public int changeUserRequestStatusInDB(int status, int requestId){
         return jdbcTemplate.update("UPDATE userRequests SET jobFinished=? WHERE id=?", status, requestId);
     }
 
@@ -57,7 +74,7 @@ public class UniqueStringGeneratorModel{
         }
 
         userRequest.setStatus(UserRequest.Status.FINISHED);
-        //changeUserRequestStatusInDB(UserRequest.Status.FINISHED, userRequest.getId());
+        changeUserRequestStatusInDB(UserRequest.Status.FINISHED, userRequest.getId());
 
         return 1;
     }
